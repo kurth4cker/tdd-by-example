413dbd74b6c39dcc143d6044550fe02a25367edc
kurth4cker <kurth4cker@gmail.com>
2025-03-16 02:29:43 +0300

    money: implement currency translation

--- money/money.go
+++ money/money.go
@@ -1,11 +1,77 @@
 package money
 
+type Sum struct {
+	Augend, Addend Expression
+}
+
+func (s Sum) Add(addend Expression) Expression {
+	return Sum{
+		Augend: s,
+		Addend: addend,
+	}
+}
+
+func (s Sum) Reduce(bank Bank, to string) Money {
+	amount := s.Augend.Reduce(bank, to).Amount +
+		s.Addend.Reduce(bank, to).Amount
+	return Money{
+		Amount:   amount,
+		Currency: to,
+	}
+}
+
+type Bank struct {
+	rates map[pair]int
+}
+
+func NewBank() Bank {
+	return Bank{
+		rates: make(map[pair]int),
+	}
+}
+
+func (b Bank) AddRate(from, to string, rate int) {
+	b.rates[pair{from, to}] = rate
+}
+
+func (b Bank) Reduce(expression Expression, currency string) Money {
+	return expression.Reduce(b, currency)
+}
+
+func (b Bank) Rate(from, to string) int {
+	if from == to {
+		return 1
+	}
+	rate := b.rates[pair{from, to}]
+	return rate
+}
+
+type Expression interface {
+	Add(addend Expression) Expression
+	Reduce(bank Bank, currency string) Money
+}
+
 type Money struct {
 	Currency string
 	Amount   int
 }
 
-func (m Money) Times(multiplier int) Money {
+func (m Money) Add(other Expression) Expression {
+	return Sum{
+		Augend: m,
+		Addend: other,
+	}
+}
+
+func (m Money) Reduce(bank Bank, to string) Money {
+	rate := bank.Rate(m.Currency, to)
+	return Money{
+		Amount:   m.Amount / rate,
+		Currency: to,
+	}
+}
+
+func (m Money) Times(multiplier int) Expression {
 	return Money{
 		Currency: m.Currency,
 		Amount:   m.Amount * multiplier,
@@ -32,4 +98,8 @@ func Franc(amount int) Money {
 
 func IsEqual(m1, m2 Money) bool {
 	return m1 == m2
+}
+
+type pair struct {
+	from, to string
 }
--- money/money_test.go
+++ money/money_test.go
@@ -7,6 +7,85 @@ import (
 	"github.com/kurth4cker/tdd-by-example/money"
 )
 
+func TestAdd(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		five := money.Dollar(5)
+		sum := five.Add(five)
+		bank := money.Bank{}
+
+		got := bank.Reduce(sum, "USD")
+		want := money.Dollar(10)
+		assert.Equal(t, got, want)
+	})
+
+	t.Run("Sum", func(t *testing.T) {
+		five := money.Dollar(5)
+		result := five.Add(five)
+		sum, ok := result.(money.Sum)
+		assert.True(t, ok)
+		assert.Equal(t, sum.Augend, money.Expression(five))
+		assert.Equal(t, sum.Addend, money.Expression(five))
+	})
+
+	t.Run("Reduce Sum", func(t *testing.T) {
+		sum := money.Sum{
+			Augend: money.Dollar(3),
+			Addend: money.Dollar(4),
+		}
+		bank := money.Bank{}
+
+		got := bank.Reduce(sum, "USD")
+		want := money.Dollar(7)
+		assert.Equal(t, got, want)
+	})
+
+	t.Run("mixed currencies", func(t *testing.T) {
+		fiveDollars := money.Expression(money.Dollar(5))
+		tenFrancs := money.Expression(money.Franc(10))
+		bank := money.NewBank()
+		bank.AddRate("CHF", "USD", 2)
+
+		got := bank.Reduce(fiveDollars.Add(tenFrancs), "USD")
+		want := money.Dollar(10)
+		assert.Equal(t, got, want)
+	})
+}
+
+func TestBank_Rate(t *testing.T) {
+	t.Run("identity rate", func(t *testing.T) {
+		got := money.NewBank().Rate("USD", "USD")
+		want := 1
+		assert.Equal(t, got, want)
+	})
+}
+
+func TestBank_Reduce(t *testing.T) {
+	bank := money.Bank{}
+	got := bank.Reduce(money.Dollar(1), "USD")
+	want := money.Dollar(1)
+	assert.Equal(t, got, want)
+}
+
+func TestMoney_Reduce(t *testing.T) {
+	bank := money.NewBank()
+	bank.AddRate("CHF", "USD", 2)
+	got := bank.Reduce(money.Franc(2), "USD")
+	want := money.Dollar(1)
+	assert.Equal(t, got, want)
+}
+
+func TestSum_Add(t *testing.T) {
+	fiveBucks := money.Dollar(5)
+	tenFrancs := money.Franc(10)
+	bank := money.NewBank()
+	bank.AddRate("CHF", "USD", 2)
+	sum := money.Sum{fiveBucks, tenFrancs}.Add(fiveBucks)
+
+	got := bank.Reduce(sum, "USD")
+	want := money.Dollar(15)
+	assert.Equal(t, got, want)
+}
+
 func TestCurrency(t *testing.T) {
 	dollar := money.Dollar(1)
 	dollarCurrency := money.Currency(dollar)
@@ -23,11 +102,11 @@ func TestMultiplication(t *testing.T) {
 
 		got := five.Times(2)
 		want := money.Dollar(10)
-		assert.Equal(t, got, want)
+		assertEqualExpression(t, got, want)
 
 		got = five.Times(3)
 		want = money.Dollar(15)
-		assert.Equal(t, got, want)
+		assertEqualExpression(t, got, want)
 	})
 
 	t.Run("Franc", func(t *testing.T) {
@@ -35,11 +114,11 @@ func TestMultiplication(t *testing.T) {
 
 		got := five.Times(2)
 		want := money.Franc(10)
-		assert.Equal(t, got, want)
+		assertEqualExpression(t, got, want)
 
 		got = five.Times(3)
 		want = money.Franc(15)
-		assert.Equal(t, got, want)
+		assertEqualExpression(t, got, want)
 	})
 }
 
@@ -52,4 +131,9 @@ func TestEquality(t *testing.T) {
 	dollar := money.Dollar(5)
 	franc := money.Franc(5)
 	assert.False(t, money.IsEqual(dollar, franc))
+}
+
+func assertEqualExpression(t *testing.T, got, want money.Expression) {
+	t.Helper()
+	assert.Equal(t, got, want)
 }
